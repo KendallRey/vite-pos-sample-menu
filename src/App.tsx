eb1e592de9ee033207faf357c69f68c7ead683a6
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-06-09 21:52:00 +0800

    added initial app routes
    - suspense (Todo)
    - lazy import (Todo)
    - error boundary (Todo)

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Providers from "./services/providers";
+import { MenuPage } from "./pages/menu/page";
+import Layout from "./components/layout/Layout";
+
+// TODOS:
+// Suspense
+// Lazy Components
+// Error Boundary
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Providers>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<MenuPage />} />
+          </Route>
+          <Route path="/about" element={<Layout />}>
+            <Route index element={<MenuPage />} />
+          </Route>
+          <Route path="/contact" element={<Layout />}>
+            <Route index element={<MenuPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Providers>
+  );
 }
 
-export default App
+export default App;
