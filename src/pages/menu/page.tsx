88a137b43866aa849d49bec4e7c479c66623f85b
UVMapping <107897863+KendallRey@users.noreply.github.com>
2024-06-17 23:22:36 +0800

    added menu item page

--- src/pages/menu/page.tsx
+++ src/pages/menu/page.tsx
@@ -1,5 +1,17 @@
 import PageContainer from "@/components/container/Page";
+import MenuPageActions from "./ui/MenuPageActions";
+import CreateMenuItemDialog from "./ui/CreateMenuItemDialog";
+import MenuItemList from "./ui/MenuItemList";
 
 export const MenuPage = () => {
-  return <PageContainer title="Menu"></PageContainer>;
+
+  return (
+    <>
+      <PageContainer title="Menu">
+        <MenuPageActions />
+        <MenuItemList/>
+      </PageContainer>
+      <CreateMenuItemDialog />
+    </>
+  );
 };
